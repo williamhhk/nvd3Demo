6b878b98f06988859d81c55ee9e506950482f3ed
williamhhk <william.han@gmail.com>
2017-09-02 13:58:54 -0700

    Initial Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+declare let d3: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+  '../../node_modules/nvd3/build/nv.d3.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  options;
+  data;
+  ngOnInit() {
+    this.options = {
+            chart: {
+              type: 'pieChart',
+              height: 350,
+            
+              donut: true,
+              x: function(d) {
+                if (d.key.length > 30) {
+                  return d.key.substr(0, 30) + "...";
+                } else {
+                  return d.key + '   ' + d.y;
+                }
+              },
+              y: function(d){return d.y;},
+              showLabels: true,
+              //pieLabelsOutside : true,
+              labelType : "value",
+              // useInteractiveGuideline: true, 
+              //showLegend : false,               
+
+              // pie: {
+              //     startAngle: function(d) { return d.startAngle -Math.PI/2 },
+              //     endAngle: function(d) { return d.endAngle -Math.PI/2 }
+              // },
+              // duration: 500,
+              // legend: {
+              //     margin: {
+              //         top: 50,
+              //         right: 0,
+              //         bottom: 50,
+              //         left: 0
+              //     }
+              // }
+
+              transitionDuration: 500,
+              legend: {
+                vers : 'furious',
+                  margin: {
+                      top: 60,
+                      right: 0,
+                      bottom: 0,
+                      left: 0
+                  }                
+              },
+
+              legendPosition: 'right',
+              valueFormat: function(d) {
+                return d;
+              }
+        } 
+    };
+
+    this.data = [
+            {
+                key: "One",
+                y: 5
+            },
+            {
+                key: "Two",
+                y: 2
+            },
+            {
+                key: "Three",
+                y: 9
+            },
+            {
+                key: "Four",
+                y: 7
+            },
+            {
+                key: "Five",
+                y: 4
+            },
+            {
+                key: "Six sadfsadfsa",
+                y: 3
+            },
+
+        ];
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NvD3Module } from 'ng2-nvd3';
+
+// d3 and nvd3 should be included somewhere
+import 'd3';
+import 'nvd3';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NvD3Module 
   ],
   providers: [],
   bootstrap: [AppComponent]
